08a3ec0661d2e61ce955cd79119d1a72781cbef9
mai095 <maiabdelmoneam095@gmail.com>
2023-09-10 22:51:13 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'about',pathMatch:'full'},
+  {path:'about',component:AboutComponent,title:"About Component"},
+  {path:'contact',component:ContactComponent, title:"Contact Component"},
+  {path:'portfolio',component:PortfolioComponent, title:"Portfolio Component"},
+
+  {path:'**',component:NotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
